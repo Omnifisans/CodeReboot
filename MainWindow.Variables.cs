170255710fdea2b16f002476328d8dd789fb9e28
Omnifisans <151912960+Omnifisans@users.noreply.github.com>
2026-04-28 18:56:10 +0500

    Update MainWindow.Variables.cs

--- MainWindow.Variables.cs
+++ MainWindow.Variables.cs
@@ -1,43 +1,40 @@
-﻿namespace CodeRebootWPF
+namespace CodeRebootWPF
 {
     public partial class MainWindow
     {
         internal bool GetExitDoorLocked() => exitDoorLocked;
-        internal void SetExitDoorLocked(bool value)
-        {
-            exitDoorLocked = value;
-            CheckAndOpenDoor();
-        }
-
+        internal void SetExitDoorLocked(bool value) { exitDoorLocked = value; CheckAndOpenDoor(); }
         internal int GetPowerLevel() => powerLevel;
         internal void SetPowerLevel(int value) => powerLevel = value;
-
         internal bool GetGeneratorActive() => generatorActive;
         internal void SetGeneratorActive(bool value) => generatorActive = value;
-
         internal int GetDoorCode() => doorCode;
-        internal void SetDoorCode(int value)
+        internal void SetDoorCode(int value) { doorCode = value; CheckAndOpenDoor(); }
+        internal string GetActivationCode() => activationCode;
+        internal void SetActivationCode(string value)
         {
-            doorCode = value;
-            CheckAndOpenDoor();
+            activationCode = value;
+            if (currentLevel == 3 && activationCode.Trim() == "DEBUG")
+            {
+                SetExitDoorLocked(false);
+            }
+            else
+            {
+                CheckAndOpenDoor();
+            }
         }
-
         internal int GetCurrentLevel() => currentLevel;
+        internal bool GetHasPiece1() => hasPiece1;
+        internal bool GetHasPiece2() => hasPiece2;
+        internal bool GetDoorSpawned() => doorSpawned;
 
         private void CheckAndOpenDoor()
         {
-            if (currentLevel == 1)
-            {
-                // На первом уровне дверь открывается只要 exitDoorLocked == false
-                if (!exitDoorLocked)
-                    OpenDoor();
-            }
-            else if (currentLevel == 2)
-            {
-                // На втором уровне нужно оба условия
-                if (!exitDoorLocked && doorCode == 42)
-                    OpenDoor();
-            }
+            if (currentLevel == 1 && !exitDoorLocked) OpenDoor();
+            else if (currentLevel == 2 && !exitDoorLocked && doorCode == 42) OpenDoor();
+            else if (currentLevel == 3 && !exitDoorLocked) OpenDoor();
+            else if (currentLevel == 4 && !exitDoorLocked) OpenDoor();
+            else if (currentLevel == 5 && !exitDoorLocked && doorSpawned) OpenDoor();
         }
     }
 }
